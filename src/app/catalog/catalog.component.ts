e57f7b310611fd2497c9e89d1dc68d509831ea19
null <null>
2024-03-27 17:21:31 -0400

    end - The NGClass... (sect 5)

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -188,6 +188,11 @@ export class CatalogComponent {
     ];
   }
 
+  getDiscountedClasses(product: IProduct) {
+    if (product.discount > 0) return ['strikethrough']
+    else return [];
+  }
+
   getImageUrl(product: IProduct) {
     if (!product) return '';
     return '/assets/images/robot-parts/' + product.imageName;
